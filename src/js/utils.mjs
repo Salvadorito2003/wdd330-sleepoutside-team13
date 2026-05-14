1f0f093e0e375e2fd4b18b6e8660a3d0fb2bb4cc
Lsonsprofile <badument@gmail.com>
2026-05-14 21:17:22 +0100

    finish the five steps of team activity

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -36,3 +36,10 @@ export function addItemToArray(product) {
   setLocalStorage("so-cart", product);
 }
 
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get('product')
+  return product;
+}
+
