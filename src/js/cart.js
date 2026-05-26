3cfe685c4e9f9216e8f35ab649b2f04ec813f06e
Tobias <tobiasaguilartarea@gmail.com>
2026-05-25 23:33:07 -0300

    fixed mistake

--- src/js/cart.js
+++ src/js/cart.js
@@ -6,6 +6,12 @@ await loadHeaderFooter();
 // Load header and footer
 loadHeaderFooter();
 
+function renderCartContents() {
+  const cartItems = getLocalStorage("so-cart");
+  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+}
+
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
