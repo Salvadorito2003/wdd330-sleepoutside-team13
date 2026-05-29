0873ef30f2afca1d88a189b68dd7197e4629070c
Tobias <tobiasaguilartarea@gmail.com>
2026-05-28 23:19:06 -0300

    Step 1 through 4 of Team Activity

---
+++ src/js/checkout-process.mjs
@@ -0,0 +1,50 @@
+export default class CheckoutProcess {
+  constructor(key, outputSelector) {
+    this.key = key;
+    this.outputSelector = outputSelector;
+    this.list = [];
+    this.itemTotal = 0;
+    this.shipping = 0;
+    this.tax = 0;
+    this.orderTotal = 0;
+  }
+
+  init() {
+    this.list = getLocalStorage(this.key);
+    this.calculateItemSummary();
+  }
+
+  calculateItemSubTotal() {
+    // calculate and display the total dollar amount of the items in the cart, and the number of items.
+    let subTotal = 0;
+    this.itemTotal = 0;
+    foreach(item in this.list)
+    {
+        subTotal += item.FinalPrice;
+        this.itemTotal++;
+    }
+    return subTotal;
+  }
+
+  calculateOrderTotal() {
+    // calculate the tax and shipping amounts. Add those to the cart total to figure out the order total
+    this.tax = (this.itemTotal * 0.06)
+    this.shipping = 8
+    foreach(item in this.list)
+    {
+        this.shipping += 2;
+    }
+    this.orderTotal = this.calculateItemSubTotal() + this.tax + this.shipping;
+
+    // display the totals.
+    this.displayOrderTotals();
+  }
+
+  displayOrderTotals() {
+    // once the totals are all calculated display them in the order summary page
+    const tax = document.querySelector(`${this.outputSelector} #tax`);
+
+
+    tax.innerText = `$${this.tax.toFixed(2)}`;
+  }
+}
